dd9b29c5b19395b614456a036bdf088dfcf4cd41
CaiQianCheng <caiqiancheng6614@163.com>
2019-01-25 19:12:48 +0800

    缓冲通道demo

--- BufferChannel.go
+++ BufferChannel.go
@@ -1,14 +1,23 @@
 package main
+import "time"
 
-var bufferedChannel chan bool
-
+var bufferedChannel chan int
 func main() {
-
 	//缓存5个的通道
-	bufferedChannel = make(chan bool, 5)
-
-
+	bufferedChannel = make(chan int, 5)
+	go testFun()
+	//读不到会阻塞
+	println(<-bufferedChannel)
+	println(<-bufferedChannel)
+	println(<-bufferedChannel)
+	println(<-bufferedChannel)
+	println(<-bufferedChannel)
 }
 
-
-
+func testFun() {
+	//写满5个才阻塞
+	for i := 0; i < 5; i++ {
+		bufferedChannel <- i
+		time.Sleep(time.Second)
+	}
+}
